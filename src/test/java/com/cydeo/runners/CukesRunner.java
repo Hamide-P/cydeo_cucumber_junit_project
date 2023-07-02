b3ac301cb72345cfab9da84ba579048fed760326
mamihami <hamide.pehlivan@hotmail.com>
2023-07-02 23:43:31 +0200

    review added

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
         dryRun = false, // if we want to execute stepDefinitions or not (false or true)
-        tags = "@ceos or @scientists",
+        tags = "@vytrackLogin",
         publish = true //generating a report with public link
 
 )
